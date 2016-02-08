066bc70438ce96d025263fd5414bd56dbd5c6f17
Holly Klose <hollyklose@gmail.com>
2016-02-08 14:10:12 -0700

    first git commit

--- Multiples-Game/ViewController.swift
+++ Multiples-Game/ViewController.swift
@@ -9,16 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var whichMultiple = 0
+    var numberAddClicks = 0
+    
+    var secondPartOfEquation = 0
+    
+    var answerEquation = 0
+    
+    
+    
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var whichMultipleTextbox: UITextField!
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var addingLabel: UILabel!
+    
+    
+    @IBAction func onPlayButtonPressed (sender: UIButton!) {
+        if  whichMultipleTextbox.text != "" {
+            whichMultiple = Int(whichMultipleTextbox.text!)!
+            
+            logo.hidden = true
+            whichMultipleTextbox.hidden = true
+            playButton.hidden = true
+            addButton.hidden = false
+            addingLabel.hidden = false
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ 
+    @IBAction func onAddButtonPressed (sender: UIButton!) {
+        numberAddClicks++
+        
+        if numberAddClicks <= 7 {
+        secondPartOfEquation = whichMultiple * numberAddClicks
+        answerEquation = whichMultiple + secondPartOfEquation
+        addingLabel.text = "\(whichMultiple) + \(secondPartOfEquation) = \(answerEquation)"
+        }
+        
+        else {
+            restartGame ()
+        }
     }
+    
+    
+    func restartGame () {
+        
+            logo.hidden = false
+            whichMultipleTextbox.hidden = false
+            playButton.hidden = false
+            addButton.hidden = true
+            addingLabel.hidden = true
+            
+            whichMultiple = 0
+            numberAddClicks = 0
+            secondPartOfEquation = 0
+            answerEquation = 0
+            whichMultipleTextbox.text = ""
+           addingLabel.text = "Press Add to Add!"
+    
+    }
+    
 
 
 }
